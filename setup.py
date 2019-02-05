443bda9955f69db6b9707d971c02e04c766c0ee2
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-02-05 09:03:10 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = open('README.md').read()
 
 setup(name='aws_lambda_decorators',
-      version='0.6',
+      version='0.7',
       description='A set of python decorators to simplify aws python lambda development',
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
