877c4c64b115e440ddfdf9a2069210239279c59b
Justas Belevicius <justas.belevicius@gridsmartercities.com>
2019-02-05 13:08:28 +0000

    Document classes

--- aws_lambda_decorators/classes.py
+++ aws_lambda_decorators/classes.py
@@ -1,3 +1,4 @@
+"""All the classes used as parameters for the decorators."""
 from aws_lambda_decorators.decoders import decode
 from aws_lambda_decorators.validators import Mandatory
 from aws_lambda_decorators.utils import is_type_in_list, is_valid_variable_name
@@ -8,21 +9,51 @@ ANNOTATIONS_END = ']'
 
 
 class ExceptionHandler:
+    """Object mapping a message to a given Exception."""
+
     def __init__(self, exception, friendly_message):
+        """
+        Set the private variables of the ExceptionHandler object.
+
+        Args:
+            exception (object|Exception): An exception to be handled.
+            friendly_message (str): Message to be logged if the exception is caught.
+        """
         self._exception = exception
         self._friendly_message = friendly_message
 
     @property
     def friendly_message(self):
+        """Getter for the message parameter."""
         return self._friendly_message
 
     @property
     def exception(self):
+        """Getter for the exception parameter."""
         return self._exception
 
 
 class Parameter:
+    """Object used for defining the path to, validation and name of the variable to be extracted from event/context."""
+
     def __init__(self, path, validators=None, func_param_index=0, var_name=None):
+        """
+        Set the private variables of the Parameter object.
+
+        Args:
+            path (str): The path to the variable we want to extract. Can use [jwt], [json] to annotate the encoding of
+                the item in the dictionary e.g.:
+                given a dictionary {
+                    "a": {
+                        "b": "{ 'c': 'hello' }",
+                    }
+                }, path to c is "a/b[json]/c"
+            validators (list): A list of validators for variable validation (e.g. Mandatory(), ValidRegex(my_regex).
+            func_param_index (int): Optional, the index of dictionary  in the function signature e.g.:
+                def fun(event, context), to extract from context func_param_index has to be 1.
+            var_name (str): Optional, the name of the variable we want to assign the extracted value to. Default is the
+                last part of the path (e.g. 'c' in the case above)
+        """
         self._func_param_index = func_param_index
         self._path = path
         self._validators = validators
@@ -30,17 +61,32 @@ class Parameter:
 
     @property
     def func_param_index(self):
+        """Getter for the func_param_index parameter."""
         return self._func_param_index
 
     @property
     def path(self):
+        """Getter for the path parameter."""
         return self._path
 
     @func_param_index.setter
     def func_param_index(self, value):
+        """Setter for the func_param_index parameter."""
         self._func_param_index = value
 
     def get_value_by_path(self, args):
+        """
+        Calculate and decode the value of the variable by the given path.
+
+        Used by the decorators.
+
+        Args:
+            args (list): list of arguments passed by the decorator. Used for extracting from a given dictionary at
+                args[func_param_index]
+
+        Raise:
+            KeyError: if the parameter does not validate.
+        """
         dict_value = args[self._func_param_index]
 
         for path_key in filter(lambda item: item != '', self.path.split(PATH_DIVIDER)):
@@ -59,6 +105,7 @@ class Parameter:
         return dict_value
 
     def validate(self, value):
+        """Check if the given value adheres to the given validation rules."""
         if not self._validators:
             return True
         for validator in self._validators:
@@ -67,12 +114,25 @@ class Parameter:
         return True
 
     def get_var_name(self):
+        """
+        Getter for the var_name parameter.
+
+        Raise:
+            SyntaxError: if the name is set and is not a valid variable name
+        """
         if self._name and not is_valid_variable_name(self._name):
             raise SyntaxError(self._name)
         return self._name
 
     @staticmethod
     def get_annotations_from_key(key):
+        """
+        Extract the key and the encoding type (annotation) from the string.
+
+        Args:
+            key (str): a combined string to extract key and annotation from. e.g. ('key[jwt]' -> 'key', 'jwt',
+                                                                                   'key'      -> 'key', None)
+        """
         if ANNOTATIONS_START in key and ANNOTATIONS_END in key:
             annotation = key[key.find(ANNOTATIONS_START) + 1:key.find(ANNOTATIONS_END)]
             return key.replace(ANNOTATIONS_START + annotation + ANNOTATIONS_END, ''), annotation
@@ -80,14 +140,25 @@ class Parameter:
 
 
 class SSMParameter:
+    """Object used for defining the key and, optionally, the variable name for ssm parameter extraction."""
+
     def __init__(self, ssm_name, var_name=None):
+        """
+        Set the private variables of the SSMParameter object.
+
+        Args:
+            ssm_name (str): Key of the variable in the AWS parameter store
+            var_name (str): Optional, the name of variable to store the extracted value to. Defaults to the ssm_name.
+        """
         self._ssm_name = ssm_name
         self._name = var_name if var_name else ssm_name
 
     def get_ssm_name(self):
+        """Getter for the ssm_name parameter."""
         return self._ssm_name
 
     def get_var_name(self):
+        """Getter for the var_name parameter."""
         if self._name and not is_valid_variable_name(self._name):
             raise SyntaxError(self._name)
         return self._name
