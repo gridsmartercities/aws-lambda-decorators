c441e1e79df1453b34e8ba09f1cc3a8ac3d395d7
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-02-07 08:12:51 +0000

    fixed import issue

--- aws_lambda_decorators/__init__.py
+++ aws_lambda_decorators/__init__.py
@@ -1,5 +1,5 @@
 from aws_lambda_decorators.classes import *  # noqa
 from aws_lambda_decorators.decoders import *  # noqa
 from aws_lambda_decorators.decorators import *  # noqa
-from aws_lambda_decorators.utils import   # noqa
+from aws_lambda_decorators.utils import *  # noqa
 from aws_lambda_decorators.validators import *  # noqa
