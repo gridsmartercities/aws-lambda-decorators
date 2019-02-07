37d08bc29e1b0f96c758fb2ce8c1e4079eae530e
Justas Belevicius <justas.belevicius@gridsmartercities.com>
2019-02-07 11:13:17 +0000

    Test extract from event example

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1,5 +1,5 @@
 import unittest
-from aws_lambda_decorators.decorators import extract
+from aws_lambda_decorators.decorators import extract, extract_from_event
 from aws_lambda_decorators.classes import Parameter
 from aws_lambda_decorators.validators import Mandatory
 
@@ -36,9 +36,9 @@ class ExamplesTests(unittest.TestCase):
         def lambda_handler(a_dictionary, another_dictionary, my_param='aDefaultValue',
                            missing_non_mandatory='I am missing', missing_mandatory=None, user_id=None):
             #  you can now access the extracted parameters directly:
-            return [my_param, missing_non_mandatory, missing_mandatory, user_id]
+            return my_param, missing_non_mandatory, missing_mandatory, user_id
 
-        self.assertEqual(['Hello!', 'I am missing', None, '123'], lambda_handler(a_dictionary, another_dictionary))
+        self.assertEqual(('Hello!', 'I am missing', None, '123'), lambda_handler(a_dictionary, another_dictionary))
 
     def test_extract_to_kwrags_example(self):
         dictionary = {
@@ -83,3 +83,22 @@ class ExamplesTests(unittest.TestCase):
             return my_param
 
         self.assertEqual('Hello!', lambda_handler(dictionary))
+
+    def test_extract_from_event_example(self):
+        event = {
+            'body': '{"my_param": "Hello!"}',
+            'headers': {
+                'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+            }
+        }
+
+        @extract_from_event(parameters=[
+            Parameter(path='/body[json]/my_param', validators=[Mandatory]),
+            # extracts a mandatory my_param from the json body of the event
+            Parameter(path='/headers/Authorization[jwt]/sub', validators=[Mandatory], var_name='user_id')
+            # extract the mandatory sub value as user_id from the authorization JWT
+        ])
+        def api_gateway_lambda_handler(event, context, my_param=None, user_id=None):
+            return my_param, user_id  # returns ('Hello!', '1234567890')
+
+        self.assertEqual(('Hello!', '1234567890'), api_gateway_lambda_handler(event, None))
