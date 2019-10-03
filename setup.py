e3c1af31e792146f90361cff2010eb199ff46d5d
jamsidedown <robertanderson1993@googlemail.com>
2019-10-03 11:54:46 +0100

    Fixed buildspec to include pylint_quotes, incremented version and updated to Beta

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = open("README.md").read()
 
 setup(name="aws-lambda-decorators",
-      version="0.33",
+      version="0.34",
       description="A set of python decorators to simplify aws python lambda development",
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setup(name="aws-lambda-decorators",
       author_email="open-source@gridsmartercities.com",
       license="MIT",
       classifiers=["Intended Audience :: Developers",
-                   "Development Status :: 3 - Alpha",
+                   "Development Status :: 4 - Beta",
                    "Programming Language :: Python :: 3",
                    "License :: OSI Approved :: MIT License",
                    "Operating System :: OS Independent",
