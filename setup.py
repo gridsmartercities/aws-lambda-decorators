6a8fed7eb2fe06648ba7489f1089a6df70d588e5
jamsidedown <robertanderson1993@googlemail.com>
2019-10-15 15:48:15 +0100

    Incremented version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 LONG_DESCRIPTION = open("README.md").read()
 
 setup(name="aws-lambda-decorators",
-      version="0.41",
+      version="0.42",
       description="A set of python decorators to simplify aws python lambda development",
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
