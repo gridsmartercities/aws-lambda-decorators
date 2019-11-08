6cd27189929e8c71d4ec9e06277e5adaa27b22eb
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-11-08 08:13:17 +0000

    added examples back into project

---
+++ examples/examples.py
@@ -0,0 +1,206 @@
+# flake8: noqa
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+from schema import Schema, Or
+from aws_lambda_decorators import extract, extract_from_event, extract_from_context, extract_from_ssm, \
+    validate, log, handle_exceptions, response_body_as_json, Parameter, SSMParameter, ValidatedParameter, \
+    ExceptionHandler, Mandatory, RegexValidator, handle_all_exceptions, cors, SchemaValidator, Maximum, Minimum, \
+    Type, EnumValidator, NonEmpty, DateValidator
+
+
+@extract(parameters=[
+            # extracts a non mandatory my_param from a_dictionary
+            Parameter(path="/parent/my_param", func_param_name="a_dictionary"),
+            # extracts a non mandatory missing_non_mandatory from a_dictionary
+            Parameter(path="/parent/missing_non_mandatory", func_param_name="a_dictionary"),
+            # does not fail as the parameter is not validated as mandatory
+            Parameter(path="/parent/missing_mandatory", func_param_name="a_dictionary"),
+            # extracts a mandatory id as "user_id" from another_dictionary
+            Parameter(path="/parent/child/id", validators=[Mandatory], var_name="user_id",
+                      func_param_name="another_dictionary")
+        ])
+def extract_example(a_dictionary, another_dictionary, my_param="aDefaultValue",
+                    missing_non_mandatory="I am missing", missing_mandatory=None, user_id=None):
+    #  you can now access the extracted parameters directly:
+    return my_param, missing_non_mandatory, missing_mandatory, user_id
+
+
+@extract(parameters=[
+            # extracts a non mandatory my_param from a_dictionary
+            Parameter(path="/parent/my_param", func_param_name="a_dictionary")
+        ])
+def extract_to_kwargs_example(a_dictionary, **kwargs):
+    return kwargs["my_param"]
+
+
+@extract(parameters=[
+            # extracts a mandatory my_param from a_dictionary
+            Parameter(path="/parent/mandatory_param", func_param_name="a_dictionary", validators=[Mandatory])
+        ])
+def extract_mandatory_param_example(a_dictionary, mandatory_param=None):
+    return "Here!"  # this part will never be reached, if the mandatory_param is missing
+
+
+@extract(parameters=[
+    # extracts two mandatory parameters from a_dictionary
+    Parameter(path="/parent/mandatory_param", func_param_name="a_dictionary", validators=[Mandatory]),
+    Parameter(path="/parent/another_mandatory_param", func_param_name="a_dictionary", validators=[Mandatory]),
+    Parameter(path="/parent/an_int", func_param_name="a_dictionary", validators=[Maximum(10), Minimum(5)])
+], group_errors=True)  # groups both errors together
+def extract_multiple_param_example(a_dictionary, mandatory_param=None, another_mandatory_param=None, an_int=0):
+    return "Here!"  # this part will never be reached, if the mandatory_param is missing
+
+
+@extract(parameters=[
+    # extracts a non mandatory my_param from a_dictionary
+    Parameter(path="/parent[json]/my_param", func_param_name="a_dictionary")
+])
+def extract_from_json_example(a_dictionary, my_param=None):
+    return my_param
+
+
+@extract_from_event(parameters=[
+            # extracts a mandatory my_param from the json body of the event
+            Parameter(path="/body[json]/my_param", validators=[Mandatory]),
+            # extract the mandatory sub value as user_id from the authorization JWT
+            Parameter(path="/headers/Authorization[jwt]/sub", validators=[Mandatory], var_name="user_id")
+        ])
+def extract_from_event_example(event, context, my_param=None, user_id=None):
+    return my_param, user_id  # returns ("Hello!", "1234567890")
+
+
+@extract_from_context(parameters=[
+    # extracts a mandatory my_param from the parent element in context
+    Parameter(path="/parent/my_param", validators=[Mandatory])
+])
+def extract_from_context_example(event, context, my_param=None):
+    return my_param  # returns "Hello!"
+
+
+@extract_from_ssm(ssm_parameters=[
+            # extracts the value of one_key from SSM as a kwarg named "one_key"
+            SSMParameter(ssm_name="one_key"),
+            # extracts another_key as a kwarg named "another"
+            SSMParameter(ssm_name="another_key", var_name="another")
+        ])
+def extract_from_ssm_example(your_func_params, one_key=None, another=None):
+    return your_func_params, one_key, another
+
+
+@validate(parameters=[
+    # validates a_param as mandatory
+    ValidatedParameter(func_param_name="a_param", validators=[Mandatory]),
+    # validates another_param as mandatory and containing only digits
+    ValidatedParameter(func_param_name="another_param", validators=[Mandatory, RegexValidator(r"\d+")]),
+    # validates param_with_schema as an object with specified schema
+    ValidatedParameter(func_param_name="param_with_schema", validators=[SchemaValidator(Schema({"a": Or(str, dict)}))])
+])
+def validate_example(a_param, another_param, param_with_schema):
+    return a_param, another_param, param_with_schema  # returns a_param, another_param, param_with_schema
+
+
+@log(parameters=True, response=True)
+def log_example(parameters):
+    return "Done!"
+
+
+@handle_exceptions(handlers=[
+            ExceptionHandler(ClientError, "Your message when a client error happens.")
+        ])
+def handle_exceptions_example():
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("your_table_name")
+    table.query(KeyConditionExpression=Key("user_id").eq("1234"))
+    # ...
+
+
+@response_body_as_json
+def response_body_as_json_example():
+    return {"statusCode": 400, "body": {"param": "hello!"}}
+
+
+@extract(parameters=[
+    # extracts a non mandatory my_param from a_dictionary
+    Parameter(path="/parent[1]/my_param", func_param_name="a_dictionary")
+])
+def extract_from_list_example(a_dictionary, my_param=None):
+    return my_param
+
+
+@handle_all_exceptions()
+def handle_all_exceptions_example():
+    test_list = [1, 2, 3]
+    test_list[5]
+    # ...
+
+
+@cors(allow_origin="*", allow_methods="POST", allow_headers="Content-Type", max_age=86400)
+def cors_example():
+    return {"statusCode": 200}
+
+
+@extract(parameters=[
+    Parameter(path="/parent/an_int", func_param_name="a_dictionary",
+              validators=[Minimum(100, "Bad value {value}: should be at least {condition}")])
+])
+def extract_minimum_param_with_custom_error_example(a_dictionary, an_int=None):
+    return {}
+
+
+@extract(parameters=[
+    Parameter(path="/params/my_param_1", func_param_name="a_dictionary"),
+    Parameter(path="/params/my_param_2", func_param_name="a_dictionary")
+])
+def extract_dictionary_example(a_dictionary, **kwargs):
+    return kwargs
+
+
+@extract(parameters=[
+    Parameter(path="/params/a_bool", func_param_name="a_dictionary", validators=[Type(bool)])
+])
+def extract_type_param(a_dictionary, a_bool=False):
+    return a_bool
+
+
+@extract(parameters=[
+    Parameter(path="/params/an_enum", func_param_name="a_dictionary", validators=[EnumValidator("Hello", "Bye")])
+])
+def extract_enum_param(a_dictionary, an_enum=None):
+    return an_enum
+
+
+@extract(parameters=[
+    Parameter(path="/params/non_empty", func_param_name="a_dictionary", validators=[NonEmpty])
+])
+def extract_non_empty_param(a_dictionary, non_empty=None):
+    return non_empty
+
+
+@extract(parameters=[
+    Parameter(path="/params/date_example", func_param_name="a_dictionary",
+              validators=[DateValidator("%Y-%m-%d %H:%M:%S")])
+])
+def extract_date_param(a_dictionary, date_example=None):
+    return date_example
+
+
+@extract(parameters=[
+    Parameter(path="/params/my_param", func_param_name="a_dictionary", transform=int)
+])
+def extract_with_transform_example(a_dictionary, my_param=None):
+    return my_param
+
+
+def to_int(arg):
+    try:
+        return int(arg)
+    except Exception:
+        raise Exception("My custom error message")
+
+
+@extract(parameters=[
+    Parameter(path="/params/my_param", func_param_name="a_dictionary", transform=to_int)
+])
+def extract_with_custom_transform_example(a_dictionary, my_param=None):
+    return {}
---
+++ examples/test_examples.py
@@ -0,0 +1,372 @@
+import unittest
+from unittest.mock import patch, MagicMock, call
+from botocore.exceptions import ClientError
+from examples.examples import (extract_example, extract_to_kwargs_example, extract_mandatory_param_example,
+                               extract_from_json_example, extract_from_event_example, extract_from_context_example,
+                               extract_from_ssm_example, validate_example, log_example, handle_exceptions_example,
+                               response_body_as_json_example, extract_from_list_example, handle_all_exceptions_example,
+                               cors_example, extract_multiple_param_example,
+                               extract_minimum_param_with_custom_error_example, extract_dictionary_example,
+                               extract_type_param, extract_enum_param, extract_non_empty_param, extract_date_param,
+                               extract_with_transform_example, extract_with_custom_transform_example)
+
+
+# pylint:disable=too-many-public-methods
+class ExamplesTests(unittest.TestCase):
+
+    def test_extract_example(self):
+        #  Given these two dictionaries:
+        a_dict = {
+            "parent": {
+                "my_param": "Hello!"
+            },
+            "other": "other value"
+        }
+        b_dict = {
+            "parent": {
+                "child": {
+                    "id": "123"
+                }
+            }
+        }
+
+        # calling the decorated extract_example:
+        response = extract_example(a_dict, b_dict)
+
+        # will return the values of the extracted parameters.
+        self.assertEqual(("Hello!", "I am missing", None, "123"), response)
+
+    def test_extract_to_kwargs_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": {
+                "my_param": "Hello!"
+            },
+            "other": "other value"
+        }
+        # we can extract "my_param".
+        response = extract_to_kwargs_example(dictionary)
+
+        # and get the value from kwargs.
+        self.assertEqual("Hello!", response)
+
+    def test_extract_missing_mandatory_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": {
+                "my_param": "Hello!"
+            },
+            "other": "other value"
+        }
+        # we can try to extract a missing mandatory parameter.
+        response = extract_mandatory_param_example(dictionary)
+
+        # but we will get an error response as it is missing and it was mandatory.
+        self.assertEqual(
+            {"statusCode": 400, "body": "{\"message\": [{\"mandatory_param\": [\"Missing mandatory value\"]}]}"},
+            response)
+
+    def test_extract_multiple_param_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": {
+                "my_param": "Hello!",
+                "an_int": 20
+            },
+            "other": "other value"
+        }
+        # we can try to extract a missing mandatory parameter.
+        response = extract_multiple_param_example(dictionary)
+
+        # but we will get an error response as it is missing and it was mandatory.
+        self.assertEqual({"statusCode": 400,
+                          "body": "{\"message\": [{\"mandatory_param\": [\"Missing mandatory value\"]}, "
+                                  "{\"another_mandatory_param\": [\"Missing mandatory value\"]}, "
+                                  "{\"an_int\": [\"'20' is greater than maximum value '10'\"]}]}"}, response)
+
+    def test_extract_not_missing_mandatory_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": {
+                "mandatory_param": "Hello!"
+            }
+        }
+
+        # we can try to extract a mandatory parameter.
+        response = extract_mandatory_param_example(dictionary)
+
+        # we will get the coded lambda response as the parameter is not missing.
+        self.assertEqual("Here!", response)
+
+    def test_extract_from_json_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": "{\"my_param\": \"Hello!\"}",
+            "other": "other value"
+        }
+
+        # we can extract from a json string by adding the [json] annotation to parent.
+        response = extract_from_json_example(dictionary)
+
+        # and we will get the value of the "my_param" parameter inside the "parent" json.
+        self.assertEqual("Hello!", response)
+
+    def test_extract_from_event_example(self):
+        # Given this API Gateway event:
+        event = {
+            "body": "{\"my_param\": \"Hello!\"}",
+            "headers": {
+                "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9l"
+                                 "IiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+            }
+        }
+
+        # we can extract "my_param" from event, and "sub" from Authorization JWT
+        # using the "extract_from_event" decorator.
+        response = extract_from_event_example(event, None)
+
+        # and return both values in the lambda response.
+        self.assertEqual(("Hello!", "1234567890"), response)
+
+    def test_extract_from_context_example(self):
+        # Given this context:
+        context = {
+            "parent": {
+                "my_param": "Hello!"
+            }
+        }
+
+        # we can extract "my_param" from context using the "extract_from_context" decorator.
+        response = extract_from_context_example(None, context)
+
+        # and return the value in the lambda response.
+        self.assertEqual("Hello!", response)
+
+    @patch("boto3.client")
+    def test_extract_from_ssm_example(self, mock_boto_client):
+        # Mocking the extraction of an SSM parameter from AWS SSM:
+        mock_ssm = MagicMock()
+        mock_ssm.get_parameters.return_value = {
+            "Parameters": [
+                {
+                    "Value": "test1",
+                    "Name": "one_key"
+                },
+                {
+                    "Value": "test2",
+                    "Name": "another_key"
+                }
+            ]
+        }
+        mock_boto_client.return_value = mock_ssm
+
+        # we can extract the value of that parameter.
+        response = extract_from_ssm_example(None)
+
+        # and return the mocked SSM parameters from the lambda.
+        self.assertEqual((None, "test1", "test2"), response)
+
+    def test_validate_example(self):
+        # We can validate non-dictionary parameters too, using the "validate" decorator.
+        response = validate_example("Hello!", "123456", {"a": {"b": "c"}})
+
+        # in this case the parameters are valid and are returned by the function.
+        self.assertEqual(("Hello!", "123456", {"a": {"b": "c"}}), response)
+
+    def test_validate_raises_exception_example(self):
+        # We can validate non-dictionary parameters too, using the "validate" decorator.
+        response = validate_example("Hello!", "123456", {"a": 123456})
+
+        # in this case at least one parameter is not valid and a 400 error is returned to the caller.
+        self.assertEqual({
+            "statusCode": 400,
+            "body": "{\"message\": [{\"param_with_schema\": [\"'{'a': 123456}' does not validate against schema"
+                    " 'Schema({'a': Or(<class 'str'>, <class 'dict'>)})'\"]}]}"}, response)
+
+    @staticmethod
+    @patch("aws_lambda_decorators.decorators.LOGGER")
+    def test_log_example(mock_logger):
+        # We can use the "log" decorator to log the parameters passed to a lambda and/or the response from the lambda.
+        log_example("Hello!")  # logs "Hello!" and "Done!"
+
+        # and check the log messages were produced.
+        mock_logger.info.assert_has_calls([
+            call("Parameters: %s", ("Hello!",)),
+            call("Response: %s", "Done!")
+        ])
+
+    @patch("boto3.resource")
+    def test_handle_exceptions_example(self, mock_dynamo):
+        # Mocking the dynamo query to return a ClientError.
+        mock_table = MagicMock()
+        client_error = ClientError({}, "")
+        mock_table.query.side_effect = client_error
+
+        mock_dynamo.return_value.Table.return_value = mock_table
+
+        # we can automatically handle the ClientError, using the "exception_handler" decorator.
+        response = handle_exceptions_example()  # noqa: pylint - assignment-from-no-return
+
+        # and return the error supplied to the caller.
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], "{\"message\": \"Your message when a client error happens.\"}")
+
+    def test_response_as_json_example(self):
+        # We can automatically json dump a body dictionary:
+        response = response_body_as_json_example()
+
+        # the response body is a string.
+        self.assertEqual("{\"param\": \"hello!\"}", response["body"])
+
+    def test_extract_from_list_example(self):
+        # Given this dictionary:
+        dictionary = {
+            "parent": [
+                {"my_param": "Hello!"},
+                {"my_param": "Bye!"}
+            ],
+            "other": "other value"
+        }
+
+        # we can extract from a json string by adding the [json] annotation to parent.
+        response = extract_from_list_example(dictionary)
+
+        # and we will get the value of the "my_param" parameter inside the "parent" correct item.
+        self.assertEqual("Bye!", response)
+
+    def test_handle_all_exceptions_example(self):
+        # we can automatically handle any exceptions, using the "handle_all_exceptions" decorator.
+        response = handle_all_exceptions_example()  # noqa: pylint - assignment-from-no-return
+
+        # and return the error to the caller.
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], "{\"message\": \"list index out of range\"}")
+
+    def test_cors(self):
+        # you can automatically add CORS headers to any function, using the "cors" decorator.
+        response = cors_example()
+
+        # the response has been decorated with the access-control cors headers.
+        self.assertEqual(response["statusCode"], 200)
+        self.assertEqual(response["headers"]["access-control-allow-origin"], "*")
+        self.assertEqual(response["headers"]["access-control-allow-methods"], "POST")
+        self.assertEqual(response["headers"]["access-control-allow-headers"], "Content-Type")
+        self.assertEqual(response["headers"]["access-control-max-age"], 86400)
+
+    def test_extract_minimum_param_with_custom_error_example(self):
+        # You can add custom error messages to all validators, and incorporate to those error messages
+        # the validated value and the validation condition.
+        response = extract_minimum_param_with_custom_error_example({"parent": {"an_int": 10}})
+
+        # The error response contains the custom error message, with the correct value to validate
+        # and condition to check.
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], "{\"message\": [{\"an_int\": [\"Bad value 10: should be at least 100\"]}]}")
+
+    def test_extract_dictionary_example(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "my_param_1": "Hello!",
+                "my_param_2": "Bye!"
+            }
+        }
+
+        # calling the decorated extract_dictionary_example:
+        response = extract_dictionary_example(a_dictionary)
+
+        # will return the extracted values in a dictionary.
+        self.assertEqual({"my_param_1": "Hello!", "my_param_2": "Bye!"}, response)
+
+    def test_extract_type_param(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "a_bool": True
+            }
+        }
+
+        # calling the decorated extract_dictionary_example:
+        response = extract_type_param(a_dictionary)
+
+        # will return the extracted values in a dictionary.
+        self.assertEqual(True, response)
+
+    def test_extract_enum_param(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "an_enum": "Bye"
+            }
+        }
+
+        # calling the decorated extract_dictionary_example:
+        response = extract_enum_param(a_dictionary)
+
+        # will return the extracted values in a dictionary.
+        self.assertEqual("Bye", response)
+
+    def test_extract_non_empty_param(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "non_empty": ["first value"]
+            }
+        }
+
+        # calling the decorated extract_dictionary_example:
+        response = extract_non_empty_param(a_dictionary)
+
+        # will return the extracted values in a dictionary.
+        self.assertEqual(["first value"], response)
+
+    def test_extract_date_param(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "date_example": "2001-01-01 00:00:00"
+            }
+        }
+
+        # calling the decorated extract_dictionary_example:
+        response = extract_date_param(a_dictionary)
+
+        # will return the extracted values in a dictionary.
+        self.assertEqual("2001-01-01 00:00:00", response)
+
+    def test_extract_with_transform(self):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "my_param": "2"
+            }
+        }
+
+        # calling the decorated extract_with_transform_example:
+        response = extract_with_transform_example(a_dictionary)
+
+        # will return the integer value 2 ("2" transform to an int)
+        self.assertEqual(2, response)
+
+    @patch("aws_lambda_decorators.decorators.LOGGER")
+    def test_extract_with_custom_transform(self, mock_logger):
+        # Given this dictionary:
+        a_dictionary = {
+            "params": {
+                "my_param": "abc"
+            }
+        }
+
+        # calling the decorated extract_with_custom_transform_example:
+        response = extract_with_custom_transform_example(a_dictionary)
+
+        # The error response contains a generic error
+        self.assertEqual(response["statusCode"], 400)
+        self.assertEqual(response["body"], "{\"message\": \"Error extracting parameters\"}")
+
+        # and the logs will contain the "My custom error message" message
+        mock_logger.error.assert_called_once_with("%s: %s in argument %s for path %s",
+                                                  "Exception",
+                                                  "My custom error message",
+                                                  "a_dictionary",
+                                                  "/params/my_param")
